0fc975e635d215667e965602189d3d12521692a0
Jakub Pitucha <vaguevans@gmail.com>
2021-01-19 19:28:42 +0100

    moved .env check and middleware to main index.js

--- src/auth.js
+++ src/auth.js
@@ -1,35 +1,6 @@
 const jwt = require("jsonwebtoken");
 const { AUTH_TYPE, AUTH_HEADER, MUST_BE_BEARER } = require("./messages");
 
-function authUserMiddleware(req, res, next) {
-  try {
-    const token = getAuthorizationToken(req.headers[AUTH_HEADER]);
-    const userDetails = getUserFromToken(token);
-    req.userDetails = userDetails;
-    return next();
-  } catch (err) {
-    if (err instanceof AuthorizationSchemeError) {
-      return res.status(400).send(MUST_BE_BEARER);
-    } else if (err instanceof AuthenticationError) {
-      return res.sendStatus(401);
-    }
-  }
-}
-
-function hasDotEnvVars() {
-  if (!process.env.MONGO_INITDB_DATABASE) return false;
-  if (!process.env.MONGO_INITDB_ROOT_USERNAME) return false;
-  if (!process.env.MONGO_INITDB_ROOT_PASSWORD) return false;
-  if (!process.env.DB_USERNAME) return false;
-  if (!process.env.DB_PASSWORD) return false;
-  if (!process.env.DB_URL) return false;
-  if (!process.env.PORT) return false;
-  if (!process.env.JWT_SECRET) return false;
-  if (!process.env.OMDB_KEY) return false;
-  if (!process.env.BASIC_USER_MOVIE_LIMIT) return false;
-  return true;
-}
-
 class AuthorizationSchemeError extends Error {}
 class AuthenticationError extends Error {}
 
@@ -54,8 +25,6 @@ function getUserFromToken(token) {
 }
 
 module.exports = {
-  authUserMiddleware,
-  hasDotEnvVars,
   getUserFromToken,
   getAuthorizationToken,
   AuthorizationSchemeError,
--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
-const { hasDotEnvVars, authUserMiddleware } = require("./auth");
+const {
+  hasDotEnvVars,
+  getAuthorizationToken,
+  getUserFromToken,
+} = require("./auth");
 const { router } = require("./routes/movies/index");
 const dbConnectionProvider = require("./db/dbConnectionProvider");
 const {
@@ -15,9 +19,38 @@ const logger = require("./logger");
 
 const swaggerDocument = YAML.load("./src/swagger.yaml");
 
+function hasDotEnvVars() {
+  if (!process.env.MONGO_INITDB_DATABASE) return false;
+  if (!process.env.MONGO_INITDB_ROOT_USERNAME) return false;
+  if (!process.env.MONGO_INITDB_ROOT_PASSWORD) return false;
+  if (!process.env.DB_USERNAME) return false;
+  if (!process.env.DB_PASSWORD) return false;
+  if (!process.env.DB_URL) return false;
+  if (!process.env.PORT) return false;
+  if (!process.env.JWT_SECRET) return false;
+  if (!process.env.OMDB_KEY) return false;
+  if (!process.env.BASIC_USER_MOVIE_LIMIT) return false;
+  return true;
+}
+
 if (!hasDotEnvVars()) {
   logger.error(INCORRECT_ENV_FILE);
   process.exit(1);
+}
+
+function authUserMiddleware(req, res, next) {
+  try {
+    const token = getAuthorizationToken(req.headers[AUTH_HEADER]);
+    const userDetails = getUserFromToken(token);
+    req.userDetails = userDetails;
+    return next();
+  } catch (err) {
+    if (err instanceof AuthorizationSchemeError) {
+      return res.status(400).send(MUST_BE_BEARER);
+    } else if (err instanceof AuthenticationError) {
+      return res.sendStatus(401);
+    }
+  }
 }
 
 dbConnectionProvider
